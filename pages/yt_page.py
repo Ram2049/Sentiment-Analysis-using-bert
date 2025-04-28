d0576ab0d617fa87559c8c5231af851b4225aca7
Ram_S <126227831+Ram2049@users.noreply.github.com>
2025-04-28 19:51:51 +0530

    Update yt_page.py

--- pages/yt_page.py
+++ pages/yt_page.py
@@ -1,21 +1,21 @@
 import streamlit as st
-from uliti import yt_predict,video_id
+from uliti import yt_predict,get_video_id
 import plotly.express as px 
 
 
 with st.container():
     link=st.text_input("Paste video link here")
     if st.button('Predict'):
-        video_Id=video_id(link)
+        video_Id=get_video_id(link)
         prediction = yt_predict(video_Id)
         st.dataframe(prediction)
    
     
-    if st.button('chart'):
-        video_Id=video_id(link)
-        prediction = yt_predict(video_Id)
-        pie_chart=px.pie(prediction,title='$$$$$',values='sentiment')
-        st.plotly_chart(pie_chart)    
+    # if st.button('chart'):
+    #     video_Id=video_id(link)
+    #     prediction = yt_predict(video_Id)
+    #     pie_chart=px.pie(prediction['sentiment'].count(),title='$$$$$',values='sentiment')
+    #     st.plotly_chart(pie_chart)    
 
     
     
